c071f21687a6c7f01fa137c07ae42065c2960118
Josh Marchán <sykopomp@sykosomatic.org>
2010-11-26 23:34:56 -0600

    Stubs for spec API implementation.

--- src/specificity.lisp
+++ src/specificity.lisp
@@ -1,2 +1,16 @@
 (in-package #:specificity)
 
+(defgeneric spec-description (spec)
+  (:documentation "Description for the spec, aka 'docstring'."))
+(defgeneric spec-name (spec)
+  (:documentation "Name for spec."))
+(defgeneric spec-expectations (spec)
+  (:documentation "Sequence of expectations."))
+(defgeneric specp (maybe-spec))
+(defmacro spec (name &body body)
+  `(progn ,name ,@body))
+
+(defun find-spec (name))
+(defun ensure-spec (name))
+(defun remove-spec (name))
+(defun run-spec (name-or-spec))
