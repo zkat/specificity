7953088531f56c3f3e618fed6180580c1bdd61a3
Kat Marchán <kzm@sykosomatic.org>
2010-11-27 15:32:12 -0600

    Expectations -> Examples

--- src/specificity.lisp
+++ src/specificity.lisp
@@ -20,16 +20,45 @@
    (expectations :initarg :expectations :reader spec-expectations))
   (:default-initargs :expectations nil))
 
-(defun ensure-spec (name &optional description)
-  (setf (gethash name *specs*) (make-instance 'spec :name name :description description)))
+(defun ensure-spec (name &key description expectations)
+  (setf (gethash name *specs*) (make-instance 'spec :name name
+                                              :description description
+                                              :expectations expectations)))
 
 (defun remove-spec (name)
   (remhash name *specs*))
 
-(defun run-spec (name-or-spec))
+(defun run-spec (name-or-spec)
+  ;; TODO
+  )
+
+(defgeneric resultp (maybe-result))
+
+(defclass result () ())
+
+(defmethod resultp ((result result)) t)
 
 (defmacro it (&body body)
   (declare (ignore body))
   `(error "Can't call IT outside of SPEC."))
 
-(defgeneric expectationp (maybe-expectation))
+;;;
+;;; Examples
+;;;
+(defun make-example (description example-function)
+  (make-instance 'example :description description :function example-function))
+
+(defgeneric examplep (maybe-example)
+  (:method ((example t)) nil)
+  (:method ((example example)) t))
+
+(defun run-example (example)
+  ;; TODO
+  )
+(defgeneric example-description (example))
+(defgeneric example-function (example)
+  (:documentation "Compiled function that represents the example's executable body."))
+
+(defclass example ()
+  ((description :initarg :description :reader example-description)
+   (function :initarg :function :reader example-function)))
